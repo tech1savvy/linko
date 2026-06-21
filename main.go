0565d41fb36e88dcca3700268c07cec03574d657
tech1savvy <amankumar010604@gmail.com>
2026-06-21 13:40:28 +0530

    feat: add env and hostname to structured logger

--- main.go
+++ main.go
@@ -37,9 +37,14 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		return 1
 	}
 
+	env := os.Getenv("ENV")
+	hostname, _ := os.Hostname()
+
 	logger = logger.With(
 		slog.String("git_sha", build.GitSHA),
 		slog.String("build_time", build.BuildTime),
+		slog.String("env", env),
+		slog.String("hostname", hostname),
 	)
 
 	defer func() {
