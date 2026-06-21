8754a4650e0d1c98940c5391844d6ad16a73f315
tech1savvy <amankumar010604@gmail.com>
2026-06-21 16:32:37 +0530

    feat: wrap request reader and response writer to log request/response body sizes, status code, and duration

--- middlewares.go
+++ middlewares.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"io"
 	"log/slog"
 	"net/http"
+	"time"
 )
+
+type spyReadCloser struct {
+	io.ReadCloser
+	bytesRead int
+}
+
+func (r *spyReadCloser) Read(p []byte) (int, error) {
+	n, err := r.ReadCloser.Read(p)
+	r.bytesRead += n
+	return n, err
+}
+
+type spyResponseWriter struct {
+	http.ResponseWriter
+	bytesWritten int
+	statusCode   int
+}
+
+func (w *spyResponseWriter) Write(p []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	n, err := w.ResponseWriter.Write(p)
+	w.bytesWritten += n
+	return n, err
+}
+
+func (w *spyResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
 
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-			logger.Info("Served request", slog.String("method", r.Method),
+			start := time.Now()
+			spyReader := &spyReadCloser{ReadCloser: r.Body}
+			spyWriter := &spyResponseWriter{ResponseWriter: w}
+			r.Body = spyReader
+			next.ServeHTTP(spyWriter, r)
+
+			logger.Info("Served request",
+				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", r.RemoteAddr),
+				slog.Duration("duration", time.Since(start)),
+				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
 			)
 		})
 	}
